7dbc4a7a5c6b1cde0ecebc1806815690341fefe4
ridwanarip <arief_09@ymail.com>
2017-06-15 02:33:22 +0700

    change console app

--- src/main/java/net/sandikta/smp/raportapp/console/App.java
+++ src/main/java/net/sandikta/smp/raportapp/console/App.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class App {
-
 	public static void main(String[] args) {
 		SessionFactory sessionFactory = new Configuration()
 				.configure().buildSessionFactory();
